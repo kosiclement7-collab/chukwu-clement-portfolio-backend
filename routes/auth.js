0bae8ab8807cfed51d17259f9804fa487c76afc7
kosiclement7-collab <kosiclement7@gmail.com>
2026-09-04 11:11:19 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const pool = require('../db'); // reuse your existing pg pool from the portfolio backend
